3108c50fff975893700704ad2898dd49a8d64df0
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:29 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '알뜰폰상품권 모바일 거래소. 소외되는 통신사 없이 알뜰폰 전용 상품권도 즉시 신속하게 매입 완료해 드립니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: 'fe5aae9212c06d12d3b1d87727c72f9e4cfb5b90',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
