c077b23ced653d19e6b50e0dc6c73297bc6a4472
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:06 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '알뜰폰상품권 희망티켓',
+  siteUrl: 'https://gift-hh-4.pages.dev',
+  siteDescription: '알뜰폰상품권 모바일 거래소. 소외되는 통신사 없이 알뜰폰 전용 상품권도 즉시 신속하게 매입 완료해 드립니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
